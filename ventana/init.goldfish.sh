7285b7158042166453cef90a04c430c6896cb6b1
Tim Harvey <tharvey@gateworks.com>
2014-08-22 15:19:08 -0700

    init: add property based abstraction for DIO gpios
    Ventana boards typically have 4 digital IO's going to a connector, but the
    Linux gpio's associated with these varies per board. To aid in creating a
    firmware image that is board-agnostic we will set an Android property
    describing the gpio for each dio as 'gpio.dio<n>'.
    
    For example, if you wish to operate on DIO0 use the value returned from
    gpio.dio0 property as the gpio in /sys/class/gpio/gpio<n>
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- ventana/init.goldfish.sh
+++ ventana/init.goldfish.sh
@@ -20,17 +20,37 @@ case "$board" in
 	GW54)
 		orientation=0
 		gps_device=/dev/ttymxc4
+		# GPIO mappings
+		setprop gpio.dio0 9
+		setprop gpio.dio1 19
+		setprop gpio.dio2 41
+		setprop gpio.dio3 42
 		;;
 	GW53)
 		orientation=3
 		gps_device=/dev/ttymxc4
+		# GPIO mappings
+		setprop gpio.dio0 16
+		setprop gpio.dio1 19
+		setprop gpio.dio2 17
+		setprop gpio.dio3 20
 		;;
 	GW52)
 		orientation=3
 		gps_device=/dev/ttymxc4
+		# GPIO mappings
+		setprop gpio.dio0 16
+		setprop gpio.dio1 19
+		setprop gpio.dio2 17
+		setprop gpio.dio3 20
 		;;
 	GW51)
 		gps_device=/dev/ttymxc0
+		# GPIO mappings
+		setprop gpio.dio0 16
+		setprop gpio.dio1 19
+		setprop gpio.dio2 17
+		setprop gpio.dio3 20
 		;;
 	*)
 		echo "unknown board: $board"
@@ -58,3 +78,25 @@ gps_present=1
 	echo "\$PSRF103,04,00,01,01*21" > /dev/gpsdevice # RMC
 	echo "\$PSRF103,05,00,01,01*20" > /dev/gpsdevice # VTG
 }
+
+# export DIO's and configure them all as inputs
+# but allow user 'system' to modify value and direction
+i=0
+while [ 1 ]; do
+	gpio=$(getprop gpio.dio${i})
+	[ "$gpio" ] || break
+
+
+	# export
+	echo ${gpio} > /sys/class/gpio/export
+	# configure as output-low
+	echo out > /sys/class/gpio/gpio${gpio}/direction
+	echo 0 > /sys/class/gpio/gpio${gpio}/value
+	# allow all users to modify value
+	chown system.system /sys/class/gpio/gpio${gpio}/value
+	chmod 0666 /sys/class/gpio/gpio${gpio}/value
+	# allow all users to modify direction
+	chown system.system /sys/class/gpio/gpio${gpio}/direction
+	chmod 0666 /sys/class/gpio/gpio${gpio}/direction
+	i=$((i+1))
+done
