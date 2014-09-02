e36e6ce174f8279adaaa852b7566ad45e4d6014a
Tim Harvey <tharvey@gateworks.com>
2014-09-02 11:37:46 -0700

    add manufacturer and brand defines

--- ventana/ventana.mk
+++ ventana/ventana.mk
@@ -7,7 +7,10 @@ $(call inherit-product-if-exists,vendor/google/products/gms.mk)
 # Overrides
 PRODUCT_NAME := ventana
 PRODUCT_DEVICE := ventana
+PRODUCT_BRAND := gateworks
+PRODUCT_MANUFACTURER := gateworks
 
+# Files to copy to the root (ramdisk) and system (rom) filesystems
 PRODUCT_COPY_FILES += \
 	device/gateworks/ventana/required_hardware.xml:system/etc/permissions/required_hardware.xml \
 	device/gateworks/ventana/init.rc:root/init.freescale.rc \
