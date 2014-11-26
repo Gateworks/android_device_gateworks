c23f21d344794d0d08bfa86f4136c9a127c2bc35
Tim Harvey <tharvey@gateworks.com>
2014-11-26 13:30:40 -0800

    update fsl media config
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- ventana/AndroidBoard.mk
+++ ventana/AndroidBoard.mk
@@ -1,8 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
-include device/fsl-proprietary/codec/fsl-codec.mk
-endif
-
+include device/fsl-proprietary/media-profile/media-profile.mk
 include device/gateworks/bootscript.mk
 include device/gateworks/ramdisk.mk
