a96bb14088ddab9ece5349f3228dc1a4a34400f6
Tim Harvey <tharvey@gateworks.com>
2015-01-16 15:15:16 -0800

    ventana: conditionally build fsl_codec
    If user has installed proprietary fsl-codec include that directory.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- ventana/AndroidBoard.mk
+++ ventana/AndroidBoard.mk
@@ -1,5 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(wildcard device/fsl-codec),)
+ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
+include device/fsl-codec/fsl-codec.mk
+endif
+endif
+
 include device/fsl-proprietary/media-profile/media-profile.mk
 include device/gateworks/bootscript.mk
 include device/gateworks/ramdisk.mk
