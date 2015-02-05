ccb48c6c7460a44118d8db339ceb1378a37f93a4
Tim Harvey <tharvey@gateworks.com>
2015-02-05 14:35:00 -0800

    ventana: add gsc_input keylayout for GSC input driver
    This provides key mapping between input keys emitted from the GSC input
    driver (such as push-button press/release) to Android key events such as
    POWER.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- ventana/ventana.mk
+++ ventana/ventana.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES +=	\
 	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
 
 PRODUCT_COPY_FILES +=	\
+	device/gateworks/common/input/gsc_input.kl:system/usr/keylayout/gsc_input.kl \
 	device/gateworks/common/input/tca8418.kl:system/usr/keylayout/tca8418.kl \
 	device/gateworks/common/input/EP0810M09.idc:system/usr/idc/EP0810M09.idc \
 	device/gateworks/common/input/TSC2007_Touchscreen.idc:system/usr/idc/TSC2007_Touchscreen.idc
