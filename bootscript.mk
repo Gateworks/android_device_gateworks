fa454323e16b477e930bf1fe7c87e9634494fde0
Tim Harvey <tharvey@gateworks.com>
2016-02-29 12:35:12 -0800

    ventana: remove bootscript/ramdisk/ubifs generation
    This needs to be moved into build/core/Makefile to properly satisfy
    build dependencies in order for multi-threaded make to work reliably.
    
    Signed-off-by: Tim Harvey <tharvey@gateworks.com>

--- bootscript.mk
+++
@@ -1,9 +0,0 @@
-BOOTSCRIPT_TARGET := $(PRODUCT_OUT)/boot/boot/6x_bootscript-ventana
-$(BOOTSCRIPT_TARGET): device/gateworks/ventana/6x_bootscript.txt $(PRODUCT_OUT)/u-boot.img
-	mkdir -p $(dir $@)
-	$(TOPDIR)bootable/bootloader/uboot-imx/tools/mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "boot script" -d $< $@
-
-.PHONY: bootscript
-bootscript: $(BOOTSCRIPT_TARGET) $(TARGET_BOOTLOADER_IMAGE)
-
-droidcore: bootscript
--- ventana/AndroidBoard.mk
+++ ventana/AndroidBoard.mk
@@ -7,6 +7,3 @@ endif
 endif
 
 include device/fsl-proprietary/media-profile/media-profile.mk
-include device/gateworks/bootscript.mk
-include device/gateworks/ramdisk.mk
-include device/gateworks/ubi.mk
