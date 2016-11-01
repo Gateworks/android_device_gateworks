39b3dfadb4ae03df32a9e3b2b79cb7a8f39f03e1
Robert Jones <rjones@gateworks.com>
2016-11-01 11:15:37 -0700

    imx6.mk: move to small language and no telephony includes
    Signed-off-by: Robert Jones <rjones@gateworks.com>

--- ventana/imx6.mk
+++ ventana/imx6.mk
@@ -1,5 +1,5 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
 $(call inherit-product, $(TOPDIR)frameworks/base/data/sounds/AllAudio.mk)
 # overrides
 PRODUCT_BRAND := Freescale
